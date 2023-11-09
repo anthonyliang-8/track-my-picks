af2fcc90206f048dedc9ce82203657a405e04af0
anthony <anthony@anthonys-Mac-mini.local>
2023-11-09 18:02:47 -0500

    Fixed wagerAmt & wagerOutcome values not showing up properly in localStorage

--- script.js
+++ script.js
@@ -8,38 +8,34 @@ var date = document.getElementById("date-placed");
 var wagerAmt = document.getElementById("wager-amt");
 var wagerOutcome = document.getElementById("wager-outcome");
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    wagerAmt.value = "";
-    wagerOutcome.value = "";
-
-    addFormEntry();
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  addFormEntry();
+  wagerAmt.value = "";
+  wagerOutcome.value = "";
 });
 
 function addFormEntry() {
-    // retrieve current localStorage data
-    // parse JSON data back to JS object
-    var currentDataEntries = JSON.parse(localStorage.getItem("formDataEntries"));
-    if(currentDataEntries == null) {
-        currentDataEntries = [];    // set to empty array if it doesn't exist
-    }
+  // retrieve current localStorage data
+  // parse JSON data back to JS object
+  var currentDataEntries = JSON.parse(localStorage.getItem("formDataEntries"));
+  if (currentDataEntries == null) {
+    currentDataEntries = []; // set to empty array if it doesn't exist
+  }
 
-    var formDataEntry = {
-        bet_type: betType.value,
-        outcome: outcome.value,
-        date: date.value,
-        wagerAmt: wagerAmt.value,
-        wagerOutcome: wagerOutcome.value
-    }
-    // set object with key "formData"
-    // use stringify to convert object into JSON string
-    localStorage.setItem("formDataEntry", JSON.stringify(formDataEntry));
-    
-    currentDataEntries.push(formDataEntry);
-    localStorage.setItem("formDataEntries", JSON.stringify(currentDataEntries));
+  var formDataEntry = {
+    bet_type: betType.value,
+    outcome: outcome.value,
+    date: date.value,
+    wagerAmt: wagerAmt.value,
+    wagerOutcome: wagerOutcome.value,
+  };
+  // set object with key "formData"
+  // use stringify to convert object into JSON string
+  localStorage.setItem("formDataEntry", JSON.stringify(formDataEntry));
 
-
+  currentDataEntries.push(formDataEntry);
+  localStorage.setItem("formDataEntries", JSON.stringify(currentDataEntries));
 }
 
 //localStorage
